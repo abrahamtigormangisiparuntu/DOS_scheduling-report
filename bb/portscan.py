7dfd280311dfb9caf8792fcd7062efa57adf0f37
don't stop learning <92497560+abrahamtigormangisiparuntu@users.noreply.github.com>
2024-01-04 09:26:26 +0700

    Add files via upload
    untuk melihat port apa yang terbuka pada web tersebut, sebelum melakukan uji coba send

---
+++ bb/portscan.py
@@ -0,0 +1,31 @@
+import socket
+import errno #buat baca error code
+import threading
+
+START_PORT = 0
+END_PORT = 5000
+opened_list =[]
+thread_list =[]
+
+def check_port(host, port):
+    global opened_list
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.settimeout(3)
+    result= s.connect_ex((host, port))
+    
+    #kalau result 0 berarti portnya open
+    if result == 0:
+        print(f"port {port} is opened")
+    # else:
+    #     print (f"{port} is closed: {errno.errorcode[result]}")
+host = "http://localhost/"
+#check_port(host, port)
+
+for port in range(START_PORT, END_PORT+1):
+    t = threading.Thread(target=check_port, args=(host, port))
+    t.start()
+    
+for t in thread_list:
+    t.join()
+    
+print(f"opened promt: {opened_list}")
